55edff0d22da03320d8b62638ba5692aa5e166a3
Anubhab2003 <anubhabchowdhury01@gmail.com>
2025-02-13 00:48:05 +0530

    admin in productions

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,7 +3,7 @@ import DeleteAllPosts from '../components/DeleteAllPosts';
 
 function AdminPage() {
     return (
-        <div className="admin-page bg-black text-green">
+        <div className="admin-page">
             <h1>Admin Page</h1>
             <DeleteAllPosts />
         </div>
